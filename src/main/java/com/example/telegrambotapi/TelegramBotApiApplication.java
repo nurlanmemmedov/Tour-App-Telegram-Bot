b9453cdccec4a40b74a3e69ddf06b817b8338b12
= <nurlanam@code.edu.az>
2021-07-06 00:15:49 +0400

    add project initial structure

---
+++ src/main/java/com/example/telegrambotapi/TelegramBotApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApiApplication.class, args);
+    }
+
+}
