8089c5a0846e74fa624c5c861d78ddaad41eb4d0
= <nurlanam@code.edu.az>
2021-07-06 10:37:15 +0400

    create entitites for Question and Action models

--- src/main/java/com/example/telegrambotapi/models/Action.java
+++ src/main/java/com/example/telegrambotapi/models/Action.java
@@ -1,11 +1,27 @@
 package com.example.telegrambotapi.models;
 
 import com.example.telegrambotapi.enums.ActionType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "actions")
 public class Action {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @ManyToOne
+    @JoinColumn(name="question_id", nullable=false)
     private Question question;
     private ActionType type;
     private String answer;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "next_id", referencedColumnName = "id")
     private Question nextQuestion;
 }
--- src/main/java/com/example/telegrambotapi/models/Question.java
+++ src/main/java/com/example/telegrambotapi/models/Question.java
@@ -1,6 +1,25 @@
 package com.example.telegrambotapi.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "questions")
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "question_text")
     private String questionText;
+    @OneToMany(mappedBy = "question",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    private List<Action> actions;
 }
